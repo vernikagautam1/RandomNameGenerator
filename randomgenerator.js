75abfd8c9f6e339be8408cc32aa6c569fc77b649
vernikagautam1 <73224790+vernikagautam1@users.noreply.github.com>
2023-02-21 15:05:44 +0530

    sql queries reduced

--- randomgenerator.js
+++ randomgenerator.js
@@ -10,39 +10,24 @@ app.use(express.json());
 app.get('/random-name', async(req, res) => {
     try {
 
-        let sql = 'SELECT id, name FROM randomname WHERE is_present = ? ORDER BY RAND() LIMIT 1'
+        let sql = 'SELECT id, name FROM randomname WHERE is_present = ?'
+            // 'SELECT id, name FROM randomname WHERE is_present = ? ORDER BY RAND() LIMIT 1'
         let value = [0]
         let [rows] = await connection.query(sql, value);
-
-        if (_.isEmpty(rows)) {
-            // If all names have been selected, reset the is_present values
+        //  console.log("outside#e@ " + _.size(rows));
+        if (_.size(rows) == 1) {
+            const name = rows[0].name;
+            console.log("id: " + rows[0].id + "   " + "name: " + rows[0].name);
+            res.json({ name: name });
             await connection.execute('UPDATE randomname SET is_present = 0 ');
-
-
-            let sql = 'SELECT id, name FROM randomname WHERE is_present = ? ORDER BY RAND() LIMIT 1'
-            let value = [0]
-            let [rows] = await connection.query(sql, value);
+        } else {
+            console.log("id: " + rows[0].id + "   " + "name: " + rows[0].name);
             const id = rows[0].id;
             const name = rows[0].name;
-
             // Update the is_present value for the selected name
             await connection.execute('UPDATE randomname SET is_present = 1 WHERE id = ?', [id]);
-
-
             res.json({ name: name });
-            return;
-
         }
-
-        console.log("id: " + rows[0].id + "   " + "name: " + rows[0].name);
-        const id = rows[0].id;
-        const name = rows[0].name;
-
-        // Update the is_present value for the selected name
-        await connection.execute('UPDATE randomname SET is_present = 1 WHERE id = ?', [id]);
-
-
-        res.json({ name: name });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Error selecting random name.' });
